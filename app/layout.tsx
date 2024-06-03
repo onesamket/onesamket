e1b54b66ce5c830f4cb9af0bc8d30131fb8df623
Tewodros Birhanu <120477045+onesamket@users.noreply.github.com>
2024-06-03 16:01:56 +0300

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import SiteHeader from "@/components/shared/site-header";
 import SiteFooter from "@/components/shared/site-footer";
 import ThemeToggle from "@/components/shared/theme-toggle";
-
+import { Metadata } from "next";
 const font = Patrick_Hand({
   subsets: ["vietnamese"],
   preload: true,
@@ -12,70 +12,21 @@ const font = Patrick_Hand({
   weight: "400",
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+
+export const metadata: Metadata = {
+  title: "Welcome to the World of Tewodros Birhanu | Explore My Journey",
+  description: "Embark on a journey through my personal website, where I showcase my experiences and passions. Join me as I share my story with the world.",
+};
+
+export default function Layout({ children }: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <head>
-        <title>
-          Welcome to the World of Tewodros Birhanu | Explore My Journey
-        </title>
-        <meta
-          name="description"
-          content="Embark on a journey through my personal website, where I showcase my experiences and passions. Join me as I share my story with the world."
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="https://x.com@onesamket" />
-        <meta
-          name="twitter:title"
-          content="Welcome to the World of Tewodros Birhanu | Explore My Journey"
-        />
-        <meta
-          name="twitter:description"
-          content="Embark on a journey through my personal website, where I showcase my experiences and passions. Join me as I share my story with the world."
-        />
-        <meta name="twitter:image" content="profile.jpg" />
-
-        <meta
-          property="og:title"
-          content="Welcome to the World of Tewodros Birhanu | Explore My Journey"
-        />
-        <meta
-          property="og:description"
-          content="Embark on a journey through my personal website, where I showcase my experiences and passions. Join me as I share my story with the world."
-        />
-        <meta property="og:image" content="profile.jpg" />
-        <meta property="og:url" content="https://onesamket.vercel.app" />
-
-        {/* LinkedIn metadata */}
-        <meta name="author" content="Tewodros Birhanu" />
-        <meta property="og:author" content="Tewodros Birhanu" />
-        <meta
-          property="og:site_name"
-          content="Tewodros Birhanu's Personal Website"
-        />
-        <meta name="linkedin:card" content="summary_large_image" />
-        <meta
-          name="linkedin:title"
-          content="Welcome to the World of Tewodros Birhanu | Explore My Journey"
-        />
-        <meta
-          name="linkedin:description"
-          content="Embark on a journey through my personal website, where I showcase my experiences and passions. Join me as I share my story with the world."
-        />
-        <meta name="linkedin:image" content="profile.jpg" />
-
-        <link rel="icon" href="profile.jpg" type="image/png" />
-      </head>
-      <body className={font.className}>
-        <SiteHeader />
-        <main>{children}</main>
-        <ThemeToggle />
-        <SiteFooter />
-      </body>
-    </html>
+    <body className={font.className}>
+      <SiteHeader />
+      <main>{children}</main>
+      <ThemeToggle />
+      <SiteFooter />
+    </body>
   );
 }
