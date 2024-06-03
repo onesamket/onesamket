be27d049040feb1cbb6fe76dfd855440fd37a28f
Tewodros Birhanu <120477045+onesamket@users.noreply.github.com>
2024-06-03 15:36:04 +0300

    Fix Vercel deployment  Page not  found error

--- app/Page.tsx
+++ app/Page.tsx
@@ -1,19 +1,5 @@
-import AboutSection from "@/components/section/about-section";
-import ContactSection from "@/components/section/contact-section";
-import ExperienceSection from "@/components/section/experience-section";
-import HeroSection from "@/components/section/hero-section";
-import ProjectSection from "@/components/section/projects-section";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <main >
-      <HeroSection />
-      <ExperienceSection />
-      <ProjectSection />
-      <div className="bg-base-300">
-        <AboutSection />
-      </div>
-      <ContactSection />
-    </main>
-  );
+export default function Page() {
+  return redirect('/tewodros')
 }
---
+++ app/tewodros/page.tsx
@@ -0,0 +1,19 @@
+import AboutSection from "@/components/section/about-section";
+import ContactSection from "@/components/section/contact-section";
+import ExperienceSection from "@/components/section/experience-section";
+import HeroSection from "@/components/section/hero-section";
+import ProjectSection from "@/components/section/projects-section";
+
+export default function HomePage() {
+    return (
+        <main >
+            <HeroSection />
+            <ExperienceSection />
+            <ProjectSection />
+            <div className="bg-base-300">
+                <AboutSection />
+            </div>
+            <ContactSection />
+        </main>
+    );
+}
