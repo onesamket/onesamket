0f40f1cf5b675bf5f9d9501b059a9d6c6e0b1e6d
Tewodros Birhanu <120477045+onesamket@users.noreply.github.com>
2024-07-19 10:25:55 +0300

    refactor  theme toggle

--- components/theme/theme-toggle.tsx
+++ components/theme/theme-toggle.tsx
@@ -9,23 +9,19 @@ export function ThemeSwitcher() {
 
   return (
     <div className="flex items-center gap-3">
-      {theme === "light" && (
-        <Button size="icon">
+      <Button size="icon" className="rounded-full">
+        {theme === "light" ? (
           <Moon
             className="cursor-pointer w-4 h-5"
             onClick={() => setTheme("dark")}
           />
-        </Button>
-      )}
-
-      {theme === "dark" && (
-        <Button size="icon">
+        ) : (
           <Sun
             className="cursor-pointer w-4 h-5"
             onClick={() => setTheme("light")}
           />
-        </Button>
-      )}
+        )}
+      </Button>
     </div>
   );
 }
