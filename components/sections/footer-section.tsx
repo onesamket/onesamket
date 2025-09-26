ca41a8ceaa1f4bfc0968a4dca5dcf6ca80a967b2
Tewodros Birhanu <onesamket@gmail.com>
2025-09-26 22:59:25 +0300

    refactor: adjust footer section layout and padding for improved responsiveness and visual balance

--- components/sections/footer-section.tsx
+++ components/sections/footer-section.tsx
@@ -3,32 +3,32 @@ import { BsTwitterX } from 'react-icons/bs';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { ThemeToggle } from '../theme-toggle';
 export default function FooterSection() {
-  return (
-    <footer className="mt-10 flex w-full items-center justify-between p-5 md:p-10 xl:px-56">
-      <div className="flex items-center space-x-1">
-        <p className="flex items-center space-x-1">
-          <span className="text-2xl">&copy;</span>
-          <span className="md:hidden">@onesamket</span>
-        </p>
-        <p className="hidden md:flex">
-          All Rights Reserved {new Date().getFullYear()}
-        </p>
-      </div>
-      <nav className="flex space-x-5" aria-label="Social media links">
-        <Link href="https://github.com/onesamket" aria-label="GitHub Profile">
-          <FaGithub className="h-4 w-4" />
-        </Link>
-        <Link href="https://x.com/onesamket" aria-label="Twitter Profile">
-          <BsTwitterX className="h-4 w-4" />
-        </Link>
-        <Link
-          href="https://www.linkedin.com/in/ln-onesamket"
-          aria-label="LinkedIn Profile"
-        >
-          <FaLinkedin className="h-4 w-4" />
-        </Link>
-      </nav>
-      <ThemeToggle />
-    </footer>
-  );
+    return (
+        <footer className="mt-10 flex w-full items-center justify-between p-5 md:p-10 xl:px-56 pb-32 lg:pb-0">
+            <div className="flex items-center space-x-1">
+                <p className="flex items-center space-x-1">
+                    <span className="text-2xl">&copy;</span>
+                    <span className="md:hidden">@onesamket</span>
+                </p>
+                <p className="hidden md:flex">
+                    All Rights Reserved {new Date().getFullYear()}
+                </p>
+            </div>
+            <nav className="flex space-x-5" aria-label="Social media links">
+                <Link href="https://github.com/onesamket" aria-label="GitHub Profile">
+                    <FaGithub className="h-4 w-4" />
+                </Link>
+                <Link href="https://x.com/onesamket" aria-label="Twitter Profile">
+                    <BsTwitterX className="h-4 w-4" />
+                </Link>
+                <Link
+                    href="https://www.linkedin.com/in/ln-onesamket"
+                    aria-label="LinkedIn Profile"
+                >
+                    <FaLinkedin className="h-4 w-4" />
+                </Link>
+            </nav>
+            <ThemeToggle />
+        </footer>
+    );
 }
